d475190a601b9bc489b49cfc0d0d791ce81d9169
JstFlip <58518901+JstFlip@users.noreply.github.com>
2022-08-21 18:26:35 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,21 @@
+import win32gui
+import win32process
+import keyboard
+import os
+import signal
+import time
+
+shortcut = 'ctrl + alt + end'
+
+def getActiveWindowPID():
+    if win32gui.GetWindowText(win32gui.GetForegroundWindow()) == 'File Explorer':
+        return
+    else:
+        window = win32gui.GetForegroundWindow()
+        tid, pid = win32process.GetWindowThreadProcessId(window)
+        os.kill(pid, signal.SIGTERM)
+        return window
+
+if __name__ == "__main__":
+    keyboard.add_hotkey(shortcut, lambda: getActiveWindowPID())
+    keyboard.wait()
